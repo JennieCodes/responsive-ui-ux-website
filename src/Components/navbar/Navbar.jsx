ffb5017ac49a2ecfee4075d35961d5492bb0d6de
Jen-nie333 <104903826+Jen-nie333@users.noreply.github.com>
2023-03-24 15:50:30 -0400

    Added pics

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 import "./navbar.css";
 
 const Navbar = () => {
   return (
     <>
-      <div>Navbar</div>
+      <div className="gpt3-navbar">
+        <div className="gpt3-navbar-links">
+          <div className="gpt3-navbar-links_logo">
+            <img src={logo} alt="logo" />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
