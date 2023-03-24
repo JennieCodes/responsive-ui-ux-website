1d88e1ac4c7196a4303c67662f73c8ad21c0b73e
Jen-nie333 <104903826+Jen-nie333@users.noreply.github.com>
2023-03-24 14:31:23 -0400

    Added containers folder with components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 import Article from "./Components/article/Article";
 import Brand from "./Components/brand/Brand";
-import CTA from "./Component/cta/CTA";
+import CTA from "./Components/cta/CTA";
 import Feature from "./Components/feature/Feature";
 import Navbar from "./Components/navbar/Navbar";
 
