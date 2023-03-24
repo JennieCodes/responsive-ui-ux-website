b82d1346d70a4d0c786344ac85bf415ffc675a90
Jen-nie333 <104903826+Jen-nie333@users.noreply.github.com>
2023-03-24 14:14:58 -0400

    Created skeleton of project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+import Article from "./Components/article/Article";
+import Brand from "./Components/brand/Brand";
+import CTA from "./Component/cta/CTA";
+import Feature from "./Components/feature/Feature";
+import Navbar from "./Components/navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <div className="App">hi</div>
+    </>
+  );
+}
+
+export default App;
