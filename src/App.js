d5372b95794dde0488f7e6188743614135b3d7f7
Jen-nie333 <104903826+Jen-nie333@users.noreply.github.com>
2023-03-24 14:41:30 -0400

    Imported all the folders into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 
-import Article from "./Components/article/Article";
-import Brand from "./Components/brand/Brand";
-import CTA from "./Components/cta/CTA";
-import Feature from "./Components/feature/Feature";
-import Navbar from "./Components/navbar/Navbar";
+import { Article, Brand, CTA, Feature, Navbar } from "./Components";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
 
 function App() {
   return (
